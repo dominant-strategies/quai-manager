723e51a51cf9805d6a974d1303893edea474afa6
Alan Orwick <alan.kev11@gmail.com>
2022-03-02 15:46:21 -0600

    Added check for reorg header length

--- manager/main.go
+++ manager/main.go
@@ -479,7 +479,9 @@ func (m *Manager) subscribeReOrgClients(client *ethclient.Client, available bool
 		case reOrgData := <-reOrgData:
 			heavier := compareReorgDifficulty(reOrgData.ReOrgHeader, reOrgData.OldChainHeaders, reOrgData.NewChainHeaders, difficultyContext)
 			if heavier {
-				m.sendReOrgHeader(reOrgData.NewChainHeaders[len(reOrgData.NewChainHeaders)-2], location)
+				if len(reOrgData.NewChainHeaders) > 1 {
+					m.sendReOrgHeader(reOrgData.NewChainHeaders[len(reOrgData.NewChainHeaders)-2], location)
+				}
 			}
 		}
 	}
