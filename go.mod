9d8b14c6fdf93d796651d65bffbbf62a6facd5b4
gop <gameofpointers@proton.me>
2022-09-26 11:41:47 -0500

    Manager updated to the latest core refactor and mining

--- go.mod
+++ go.mod
@@ -3,10 +3,11 @@ module github.com/spruce-solutions/quai-manager
 go 1.16
 
 require (
-	github.com/TwiN/go-color v1.1.0 // indirect
-	github.com/edsrzf/mmap-go v1.0.0 // indirect
-	github.com/fatih/color v1.9.0
-	github.com/hashicorp/golang-lru v0.5.5-0.20210104140557-80c98217689d
+	github.com/TwiN/go-color v1.1.0
+	github.com/dominant-strategies/go-quai v0.0.0-20220926163208-ca33f4abb3a4
+	github.com/karalabe/usb v0.0.0-20211005121534-4c5740d64559 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/spf13/viper v1.9.0
-	github.com/spruce-solutions/go-quai v0.1.0-pre.2.0.20220909134717-79e5d9600972
+	github.com/syndtr/goleveldb v1.0.1-0.20210819022825-2ae1ddf74ef7 // indirect
+	golang.org/x/time v0.0.0-20210220033141-f8bda1e9f3ba // indirect
 )
--- manager/main.go
+++ manager/main.go
@@ -17,12 +17,10 @@ import (
 	"time"
 
 	"github.com/TwiN/go-color"
-	"github.com/spruce-solutions/go-quai/common"
-	"github.com/spruce-solutions/go-quai/common/hexutil"
-	"github.com/spruce-solutions/go-quai/consensus/blake3"
-	"github.com/spruce-solutions/go-quai/core/types"
-	"github.com/spruce-solutions/go-quai/crypto"
-	"github.com/spruce-solutions/go-quai/ethclient"
+	"github.com/dominant-strategies/go-quai/common"
+	"github.com/dominant-strategies/go-quai/consensus/blake3pow"
+	"github.com/dominant-strategies/go-quai/core/types"
+	"github.com/dominant-strategies/go-quai/ethclient"
 	"github.com/spruce-solutions/quai-manager/manager/util"
 )
 
@@ -57,17 +55,14 @@ func init() {
 }
 
 type Manager struct {
-	engine *blake3.Blake3
+	engine *blake3pow.Blake3pow
 	config util.Config
 
 	header *types.Header
 
-	orderedBlockClients orderedBlockClients   // will hold all chain URLs and settings in order from prime to zone-3-3
-	pendingBlocks       []*types.ReceiptBlock // Current pending blocks of the manager
+	orderedBlockClients orderedBlockClients // will hold all chain URLs and settings in order from prime to zone-3-3
 	lock                sync.Mutex
 	location            []byte
-
-	pendingBlockCh chan *types.ReceiptBlock
 
 	updatedCh chan *types.Header
 	resultCh  chan *types.Header
@@ -181,40 +176,18 @@ func main() {
 		log.Println(color.Ize(color.Red, "Manual mode started"))
 	}
 
-	header := &types.Header{
-		ParentHash:        make([]common.Hash, 3),
-		Number:            make([]*big.Int, 3),
-		Extra:             make([][]byte, 3),
-		Time:              uint64(0),
-		BaseFee:           make([]*big.Int, 3),
-		GasLimit:          make([]uint64, 3),
-		Coinbase:          make([]common.Address, 3),
-		Difficulty:        make([]*big.Int, 3),
-		NetworkDifficulty: make([]*big.Int, 3),
-		Root:              make([]common.Hash, 3),
-		TxHash:            make([]common.Hash, 3),
-		UncleHash:         make([]common.Hash, 3),
-		ReceiptHash:       make([]common.Hash, 3),
-		GasUsed:           make([]uint64, 3),
-		Bloom:             make([]types.Bloom, 3),
+	header := types.EmptyHeader()
+
+	blake3Config := blake3pow.Config{
+		NotifyFull: true,
 	}
 
-	blake3Config := blake3.Config{
-		MiningThreads: 0,
-		NotifyFull:    true,
-	}
-
-	blake3Engine, err := blake3.New(blake3Config, nil, false)
-	if nil != err {
-		log.Fatal("Failed to create Blake3 engine: ", err)
-	}
+	blake3Engine := blake3pow.New(blake3Config, nil, false)
 
 	m := &Manager{
 		engine:              blake3Engine,
 		orderedBlockClients: allClients,
 		header:              header,
-		pendingBlocks:       make([]*types.ReceiptBlock, 3),
-		pendingBlockCh:      make(chan *types.ReceiptBlock, resultQueueSize),
 		resultCh:            make(chan *types.Header, resultQueueSize),
 		updatedCh:           make(chan *types.Header, resultQueueSize),
 		exitCh:              make(chan struct{}),
@@ -230,7 +203,7 @@ func main() {
 	if config.Mine {
 		log.Println("Starting manager in location ", config.Location)
 
-		m.fetchPendingHeader(m.orderedBlockClients.zoneClients[m.location[0]-1][m.location[1]-1])
+		m.fetchPendingHeader(m.orderedBlockClients.zoneClients[m.location[0]][m.location[1]])
 
 		// subscribing to the zone pending header update.
 		m.subscribeSlicePendingHeader()
@@ -410,15 +383,15 @@ func (m *Manager) fetchPendingHeader(client *ethclient.Client) {
 // check if the header is null. If so, don't start mining.
 func (m *Manager) headerNullCheck(header *types.Header) error {
 	err := errors.New("header has nil value, cannot continue with mining")
-	if header.Number[0] == nil {
+	if header.Number(0) == nil {
 		log.Println("Waiting to retrieve Prime header information...")
 		return err
 	}
-	if header.Number[1] == nil {
+	if header.Number(1) == nil {
 		log.Println("Waiting to retrieve Region header information...")
 		return err
 	}
-	if header.Number[2] == nil {
+	if header.Number(2) == nil {
 		log.Println("Waiting to retrieve Zone header information...")
 		return err
 	}
@@ -451,24 +424,24 @@ func (m *Manager) miningLoop() error {
 			m.lock.Lock()
 			m.lock.Unlock()
 
-			if !bytes.Equal(header.Location, m.location) {
+			if !bytes.Equal(header.Location(), m.location) {
 				continue
 			}
 			headerNull := m.headerNullCheck(header)
 			if headerNull == nil {
-				if header.Number[0].Cmp(m.previousNumber[0]) != 0 || header.Number[1].Cmp(m.previousNumber[1]) != 0 || header.Number[2].Cmp(m.previousNumber[2]) != 0 {
-					if header.Number[0].Cmp(m.previousNumber[0]) != 0 {
-						log.Println("Mining Block:  ", fmt.Sprintf("[%s %s %s]", color.Ize(color.Red, fmt.Sprint(header.Number[0])), header.Number[1].String(), header.Number[2].String()), "location", m.location, "difficulty", header.Difficulty)
-					} else if header.Number[1].Cmp(m.previousNumber[1]) != 0 {
-						log.Println("Mining Block:  ", fmt.Sprintf("[%s %s %s]", header.Number[0].String(), color.Ize(color.Yellow, fmt.Sprint(header.Number[1])), header.Number[2].String()), "location", m.location, "difficulty", header.Difficulty)
+				if header.Number(0).Cmp(m.previousNumber[0]) != 0 || header.Number(1).Cmp(m.previousNumber[1]) != 0 || header.Number(2).Cmp(m.previousNumber[2]) != 0 {
+					if header.Number(0).Cmp(m.previousNumber[0]) != 0 {
+						log.Println("Mining Block:  ", fmt.Sprintf("[%s %s %s]", color.Ize(color.Red, fmt.Sprint(header.Number(0))), header.Number(1).String(), header.Number(2).String()), "location", m.location, "difficulty", header.DifficultyArray())
+					} else if header.Number(1).Cmp(m.previousNumber[1]) != 0 {
+						log.Println("Mining Block:  ", fmt.Sprintf("[%s %s %s]", header.Number(0).String(), color.Ize(color.Yellow, fmt.Sprint(header.Number(1))), header.Number(2).String()), "location", m.location, "difficulty", header.DifficultyArray())
 					} else {
-						log.Println("Mining Block:  ", header.Number, "location", m.location, "difficulty", header.Difficulty)
-
+						log.Println("Mining Block:  ", header.NumberArray(), "location", m.location, "difficulty", header.DifficultyArray())
 					}
-					m.previousNumber = header.Number
+					m.previousNumber = header.NumberArray()
 				}
 
-				if err := m.engine.SealHeader(header, m.resultCh, stopCh); err != nil {
+				header.SetTime(uint64(time.Now().Unix()))
+				if err := m.engine.Seal(header, m.resultCh, stopCh); err != nil {
 					log.Println("Block sealing failed", "err", err)
 				}
 			}
@@ -484,7 +457,7 @@ func (m *Manager) SubmitHashRate() {
 	randomId := rand.Int()
 	randomIdArray := make([]byte, 8)
 	binary.LittleEndian.PutUint64(randomIdArray, uint64(randomId))
-	id := crypto.Keccak256Hash(randomIdArray)
+	// id := crypto.Keccak256Hash(randomIdArray)
 
 	var null float64 = 0
 	go func() {
@@ -494,7 +467,7 @@ func (m *Manager) SubmitHashRate() {
 				hashRate := m.engine.Hashrate()
 				if hashRate != null {
 					log.Println("Quai Miner  :   Hashes per second: ", hashRate)
-					m.engine.SubmitHashrate(hexutil.Uint64(hashRate), id)
+					// m.engine.SubmitHashrate(hexutil.Uint64(hashRate), id)
 				}
 			}
 		}
@@ -508,21 +481,21 @@ func (m *Manager) resultLoop() error {
 		case header := <-m.resultCh:
 			m.lock.Lock()
 
-			context, err := m.engine.GetDifficultyOrder(header)
+			context, err := m.GetDifficultyOrder(header)
 			if err != nil {
 				log.Println("Block mined has an invalid order")
 			}
 
 			if context == 0 {
-				log.Println(color.Ize(color.Red, "PRIME block :  "), header.Number, header.Hash())
+				log.Println(color.Ize(color.Red, "PRIME block :  "), header.NumberArray(), header.Hash())
 			}
 
 			if context == 1 {
-				log.Println(color.Ize(color.Yellow, "REGION block:  "), header.Number, header.Hash())
+				log.Println(color.Ize(color.Yellow, "REGION block:  "), header.NumberArray(), header.Hash())
 			}
 
 			if context == 2 {
-				log.Println(color.Ize(color.Blue, "Zone block  :  "), header.Number, header.Hash())
+				log.Println(color.Ize(color.Blue, "Zone block  :  "), header.NumberArray(), header.Hash())
 			}
 
 			// Check to see that all nodes are running before sending blocks to them.
@@ -533,7 +506,7 @@ func (m *Manager) resultLoop() error {
 
 			// Check proper difficulty for which nodes to send block to
 			// Notify blocks to put in cache before assembling new block on node
-			if context == 0 && header.Number[0] != nil {
+			if context == 0 && header.Number(0) != nil {
 				var wg sync.WaitGroup
 				wg.Add(1)
 				go m.SendMinedHeader(2, header, &wg)
@@ -545,7 +518,7 @@ func (m *Manager) resultLoop() error {
 			}
 
 			// If Region difficulty send to Region
-			if context == 1 && header.Number[1] != nil {
+			if context == 1 && header.Number(1) != nil {
 				var wg sync.WaitGroup
 				wg.Add(1)
 				go m.SendMinedHeader(2, header, &wg)
@@ -555,7 +528,7 @@ func (m *Manager) resultLoop() error {
 			}
 
 			// If Zone difficulty send to Zone
-			if context == 2 && header.Number[2] != nil {
+			if context == 2 && header.Number(2) != nil {
 				var wg sync.WaitGroup
 				wg.Add(1)
 				go m.SendMinedHeader(2, header, &wg)
@@ -572,27 +545,27 @@ func (m *Manager) allChainsOnline() bool {
 	if !checkConnection(m.orderedBlockClients.primeClient) {
 		return false
 	}
-	if !checkConnection(m.orderedBlockClients.regionClients[m.location[0]-1]) {
-		regionURL := m.config.RegionURLs[m.location[0]-1]
+	if !checkConnection(m.orderedBlockClients.regionClients[m.location[0]]) {
+		regionURL := m.config.RegionURLs[m.location[0]]
 		if regionURL != "" {
 			regionClient, err := ethclient.Dial(regionURL)
 			if err != nil {
 				log.Println("Unable to connect to node:", "Region ", m.location, regionURL)
 				return false
 			} else {
-				m.orderedBlockClients.regionClients[m.location[0]-1] = regionClient
+				m.orderedBlockClients.regionClients[m.location[0]] = regionClient
 			}
 		}
 	}
-	if !checkConnection(m.orderedBlockClients.zoneClients[m.location[0]-1][m.location[1]-1]) {
-		zoneURL := m.config.ZoneURLs[m.location[0]-1][m.location[1]-1]
+	if !checkConnection(m.orderedBlockClients.zoneClients[m.location[0]][m.location[1]]) {
+		zoneURL := m.config.ZoneURLs[m.location[0]][m.location[1]]
 		if zoneURL != "" {
 			zoneClient, err := ethclient.Dial(zoneURL)
 			if err != nil {
-				log.Println("Unable to connect to node:", "Region ", m.location[0]-1, "Zone ", m.location[1]-1, zoneURL)
+				log.Println("Unable to connect to node:", "Region ", m.location[0], "Zone ", m.location[1], zoneURL)
 				return false
 			} else {
-				m.orderedBlockClients.zoneClients[m.location[0]-1][m.location[1]-1] = zoneClient
+				m.orderedBlockClients.zoneClients[m.location[0]][m.location[1]] = zoneClient
 			}
 		}
 	}
@@ -605,10 +578,10 @@ func (m *Manager) SendMinedHeader(mined int, header *types.Header, wg *sync.Wait
 		m.orderedBlockClients.primeClient.ReceiveMinedHeader(context.Background(), header)
 	}
 	if mined == 1 {
-		m.orderedBlockClients.regionClients[m.location[0]-1].ReceiveMinedHeader(context.Background(), header)
+		m.orderedBlockClients.regionClients[m.location[0]].ReceiveMinedHeader(context.Background(), header)
 	}
 	if mined == 2 {
-		m.orderedBlockClients.zoneClients[m.location[0]-1][m.location[1]-1].ReceiveMinedHeader(context.Background(), header)
+		m.orderedBlockClients.zoneClients[m.location[0]][m.location[1]].ReceiveMinedHeader(context.Background(), header)
 	}
 	defer wg.Done()
 }
@@ -627,18 +600,17 @@ func checkConnection(client *ethclient.Client) bool {
 
 func (m *Manager) subscribeSliceHeaderRoots() {
 	go m.subscribeHeaderRoots(m.orderedBlockClients.primeClient, 0)
-	go m.subscribeHeaderRoots(m.orderedBlockClients.regionClients[m.location[0]-1], 1)
-	go m.subscribeHeaderRoots(m.orderedBlockClients.zoneClients[m.location[0]-1][m.location[1]-1], 2)
+	go m.subscribeHeaderRoots(m.orderedBlockClients.regionClients[m.location[0]], 1)
+	go m.subscribeHeaderRoots(m.orderedBlockClients.zoneClients[m.location[0]][m.location[1]], 2)
 }
 
 func (m *Manager) subscribeSlicePendingHeader() {
 	go m.subscribePendingHeader(m.orderedBlockClients.primeClient, 0)
-	go m.subscribePendingHeader(m.orderedBlockClients.regionClients[m.location[0]-1], 1)
-	go m.subscribePendingHeader(m.orderedBlockClients.zoneClients[m.location[0]-1][m.location[1]-1], 2)
+	go m.subscribePendingHeader(m.orderedBlockClients.regionClients[m.location[0]], 1)
+	go m.subscribePendingHeader(m.orderedBlockClients.zoneClients[m.location[0]][m.location[1]], 2)
 }
 
 func (m *Manager) subscribeHeaderRoots(client *ethclient.Client, index int) {
-	fmt.Println("subscribing to: ", index)
 	headerRoots := make(chan types.HeaderRoots, 1)
 	// subscribe to the header roots update
 	sub, err := client.SubscribeHeaderRoots(context.Background(), headerRoots)
@@ -652,9 +624,9 @@ func (m *Manager) subscribeHeaderRoots(client *ethclient.Client, index int) {
 		select {
 		case headerRoots := <-headerRoots:
 			m.lock.Lock()
-			m.header.Root[index] = headerRoots.StateRoot
-			m.header.TxHash[index] = headerRoots.TxsRoot
-			m.header.ReceiptHash[index] = headerRoots.ReceiptsRoot
+			m.header.SetRoot(headerRoots.StateRoot, index)
+			m.header.SetTxHash(headerRoots.TxsRoot)
+			m.header.SetReceiptHash(headerRoots.ReceiptsRoot)
 
 			// send the updated header roots to mine
 			m.updatedCh <- m.header
@@ -663,4 +635,26 @@ func (m *Manager) subscribeHeaderRoots(client *ethclient.Client, index int) {
 			break
 		}
 	}
+}
+
+var (
+	big2e256 = new(big.Int).Exp(big.NewInt(2), big.NewInt(256), big.NewInt(0)) // 2^256
+)
+
+// This function determines the difficulty order of a block
+func (m *Manager) GetDifficultyOrder(header *types.Header) (int, error) {
+	if header == nil {
+		return common.HierarchyDepth, errors.New("no header provided")
+	}
+	blockhash := m.engine.SealHash(header)
+
+	for i, difficulty := range header.DifficultyArray() {
+		if difficulty != nil && big.NewInt(0).Cmp(difficulty) < 0 {
+			target := new(big.Int).Div(big2e256, difficulty)
+			if new(big.Int).SetBytes(blockhash.Bytes()).Cmp(target) <= 0 {
+				return i, nil
+			}
+		}
+	}
+	return -1, errors.New("block does not satisfy minimum difficulty")
 }
