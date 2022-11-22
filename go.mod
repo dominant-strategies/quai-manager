a0ac55368700e4fcba7c59734186023af74dc2de
Jonathan Downing <jonathan@dominantstrategies.io>
2022-11-22 16:43:29 -0600

    Transaction test connects to all nodes and gets a key for each. 3 ETXs sent from one zone to another

--- go.mod
+++ go.mod
@@ -3,11 +3,13 @@ module github.com/spruce-solutions/quai-manager
 go 1.16
 
 require (
-	github.com/TwiN/go-color v1.1.0
+	github.com/TwiN/go-color v1.4.0
 	github.com/dominant-strategies/go-quai v0.0.0-20221121183755-054ed95d776e
-	github.com/karalabe/usb v0.0.0-20211005121534-4c5740d64559 // indirect
-	github.com/mattn/go-colorable v0.1.8 // indirect
-	github.com/spf13/viper v1.9.0
+	github.com/dominant-strategies/quai-accounts v0.0.0-20221018202534-b7e8dff9f664 // indirect
+	github.com/karalabe/usb v0.0.2 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mitchellh/go-testing-interface v1.0.0 // indirect
+	github.com/spf13/viper v1.14.0
 	github.com/syndtr/goleveldb v1.0.1-0.20210819022825-2ae1ddf74ef7 // indirect
-	golang.org/x/time v0.0.0-20210220033141-f8bda1e9f3ba // indirect
+	golang.org/x/time v0.2.0 // indirect
 )
--- transaction_test.go
+++ transaction_test.go
@@ -8,7 +8,6 @@ import (
 	"math/big"
 	"os"
 	"path/filepath"
-	"strings"
 	"testing"
 
 	"github.com/dominant-strategies/go-quai/common"
@@ -16,81 +15,231 @@ import (
 	"github.com/dominant-strategies/go-quai/crypto"
 	"github.com/dominant-strategies/go-quai/params"
 	"github.com/dominant-strategies/go-quai/quaiclient/ethclient"
-	"github.com/dominant-strategies/quai-faucet/keystore"
-	"github.com/spruce-solutions/go-quai/log"
+	accounts "github.com/dominant-strategies/quai-accounts"
+	"github.com/dominant-strategies/quai-accounts/keystore"
+	"github.com/holiman/uint256"
+	"github.com/spruce-solutions/quai-manager/manager/util"
 )
 
 var BASEFEE = common.Big1
 var MINERTIP = common.Big1
-var GAS = uint64(100000)
+var GAS = uint64(110000)
 var VALUE = big.NewInt(params.Ether)
-var PORT = "8547"
+var PARAMS = params.RopstenChainConfig
+var numChains = 13
+var chainList = []string{"prime", "cyprus", "cyprus1", "cyprus2", "cyprus3", "paxos", "paxos1", "paxos2", "paxos3", "hydra", "hydra1", "hydra2", "hydra3"}
+var from_zone = 0
 
 func TestOpETX(t *testing.T) {
-	contract, err := hex.DecodeString("60806040526000806000806000600180620186a06706f05b59d3b20000735a457339697cb56e5a9bfa5267ea80d2c6375d986000f690505060698060446000396000f3fe6080604052600080fdfea2646970667358221220d51a551dccdf100829b6e928e928be7c4b803430100a5e618686c4ab3d17280964736f6c63782c302e382e31382d646576656c6f702e323032322e31312e372b636f6d6d69742e32636336363130652e6d6f64005d")
+	config, err := util.LoadConfig(".")
 	if err != nil {
-		t.Error(err.Error())
+		t.Error("cannot load config: " + err.Error())
 		t.Fail()
 	}
-	signer := types.LatestSigner(params.MainnetChainConfig)
-	testKey, _ := crypto.HexToECDSA("b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291") // Private Key
-	addr := crypto.PubkeyToAddress(testKey.PublicKey)
-	common.NodeLocation = *addr.Location() // Assuming we are in the same location as the provided key
-	primeClient, err := ethclient.Dial("ws://127.0.0.1:" + PORT)
+	allClients := getNodeClients(config)
+
+	contract, err := hex.DecodeString("60806040526040516101e03803806101e0833981810160405281019061002591906100dc565b600080600080600086888a8c8e6000f6905050505050505050610169565b600080fd5b600073ffffffffffffffffffffffffffffffffffffffff82169050919050565b600061007382610048565b9050919050565b61008381610068565b811461008e57600080fd5b50565b6000815190506100a08161007a565b92915050565b6000819050919050565b6100b9816100a6565b81146100c457600080fd5b50565b6000815190506100d6816100b0565b92915050565b60008060008060008060c087890312156100f9576100f8610043565b5b600061010789828a01610091565b965050602061011889828a016100c7565b955050604061012989828a016100c7565b945050606061013a89828a016100c7565b935050608061014b89828a016100c7565b92505060a061015c89828a016100c7565b9150509295509295509295565b6069806101776000396000f3fe6080604052600080fdfea2646970667358221220ae701927ce1c6a30dbd24ad1b79952d125849aa7fd08aaa826c17c489699f20764736f6c63782c302e382e31382d646576656c6f702e323032322e31312e382b636f6d6d69742e36306161353861362e6d6f64005d")
 	if err != nil {
 		t.Error(err.Error())
 		t.Fail()
 	}
 
 	ks := keystore.NewKeyStore(filepath.Join(os.Getenv("HOME"), ".faucet", "keys"), keystore.StandardScryptN, keystore.StandardScryptP)
-	// Load up the account keys and decrypt its password
-	blob, err := ioutil.ReadFile(*accPassFlag)
-	if err != nil {
-		log.Crit("Failed to read account password contents", "file", *accPassFlag, "err", err)
-	}
-	pass := strings.TrimSuffix(string(blob), "\n")
+	pass := ""
 
-	for i := 0; i < *numChainsFlag; i++ {
-		if blob, err = ioutil.ReadFile("keystore/" + chainList[i] + ".json"); err != nil {
-			log.Crit("Failed to read account key contents", "file", chainList[i]+".json", "err", err)
+	for i := 0; i < numChains; i++ {
+		blob, err := ioutil.ReadFile("keystore/" + chainList[i] + ".json") // put keystore folder in the current directory
+		if err != nil {
+			t.Error("Failed to read account key contents", "file", chainList[i]+".json", "err", err)
+			t.Fail()
 		}
-		fmt.Println("Here", i)
 		acc, err := ks.Import(blob, pass, pass)
 		if err != nil && err != keystore.ErrAccountAlreadyExists {
-			log.Crit("Failed to import faucet signer account", "err", err)
+			t.Error("Failed to import faucet signer account", "err", err)
+			t.Fail()
 		}
 		if err := ks.Unlock(acc, pass); err != nil {
-			log.Crit("Failed to unlock faucet signer account", "err", err)
+			t.Error("Failed to unlock faucet signer account", "err", err)
+			t.Fail()
+		}
+		addAccToClient(&allClients, acc, i)
+	}
+
+	for i := 0; i < 3; i++ {
+		if !allClients.zonesAvailable[from_zone][i] {
+			continue
+		}
+		client := allClients.zoneClients[from_zone][i]
+		from := allClients.zoneAccounts[from_zone][i]
+		common.NodeLocation = *from.Address.Location() // Assuming we are in the same location as the provided key
+
+		to, err := uint256.FromHex(allClients.zoneAccounts[from_zone+1][i].Address.Hex())
+		if err != nil {
+			t.Error(err.Error())
+			t.Fail()
+		}
+		amount, _ := uint256.FromBig(VALUE)
+		limit := uint256.NewInt(GAS)
+		tip, _ := uint256.FromBig(MINERTIP)
+		baseFee, _ := uint256.FromBig(BASEFEE)
+		data := make([]byte, 0, 0)
+		temp := to.Bytes32()
+		data = append(data, temp[:]...)
+		temp = amount.Bytes32()
+		data = append(data, temp[:]...)
+		temp = limit.Bytes32()
+		data = append(data, temp[:]...)
+		temp = tip.Bytes32()
+		data = append(data, temp[:]...)
+		temp = baseFee.Bytes32()
+		data = append(data, temp[:]...)
+		contract = append(contract, data...)
+
+		nonce, err := client.NonceAt(context.Background(), from.Address, nil)
+		if err != nil {
+			t.Error(err.Error())
+			t.Fail()
+		}
+		i := uint8(0)
+		temp = uint256.NewInt(uint64(i)).Bytes32()
+		contract = append(contract, temp[:]...)
+		for {
+			contract[len(contract)-1] = i // one byte (8 bits) for contract nonce is sufficient
+			contractAddr := crypto.CreateAddress(from.Address, nonce, contract)
+			if contractAddr.IsInChainScope() {
+				break
+			}
+			i++
+		}
+
+		inner_tx := types.InternalTx{ChainID: PARAMS.ChainID, Nonce: nonce, GasTipCap: MINERTIP, GasFeeCap: BASEFEE, Gas: GAS, To: nil, Value: VALUE, Data: contract}
+		tx, err := ks.SignTx(from, types.NewTx(&inner_tx), PARAMS.ChainID)
+		if err != nil {
+			t.Error(err.Error())
+			t.Fail()
+		}
+
+		err = client.SendTransaction(context.Background(), tx)
+		if err != nil {
+			t.Error(err.Error())
+			t.Fail()
 		}
 	}
 
-	i := uint8(0)
-	nonce := uint64(0)
-	contract = append(contract, i)
-	for {
-		contract[len(contract)-1] = i
-		contractAddr := crypto.CreateAddress(addr, nonce, contract)
-		if contractAddr.IsInChainScope() {
-			break
+}
+
+// Block struct to hold all Client fields.
+type orderedBlockClients struct {
+	primeClient      *ethclient.Client
+	primeAvailable   bool
+	primeAccount     accounts.Account
+	regionClients    []*ethclient.Client
+	regionsAvailable []bool
+	regionAccounts   []accounts.Account
+	zoneClients      [][]*ethclient.Client
+	zonesAvailable   [][]bool
+	zoneAccounts     [][]accounts.Account
+}
+
+// getNodeClients takes in a config and retrieves the Prime, Region, and Zone client
+// that is used for mining in a slice.
+func getNodeClients(config util.Config) orderedBlockClients {
+
+	// initializing all the clients
+	allClients := orderedBlockClients{
+		primeAvailable:   false,
+		regionClients:    make([]*ethclient.Client, 3),
+		regionsAvailable: make([]bool, 3),
+		regionAccounts:   make([]accounts.Account, 3),
+		zoneClients:      make([][]*ethclient.Client, 3),
+		zonesAvailable:   make([][]bool, 3),
+		zoneAccounts:     make([][]accounts.Account, 3),
+	}
+
+	for i := range allClients.zoneClients {
+		allClients.zoneClients[i] = make([]*ethclient.Client, 3)
+	}
+	for i := range allClients.zonesAvailable {
+		allClients.zonesAvailable[i] = make([]bool, 3)
+	}
+	for i := range allClients.zoneClients {
+		allClients.zoneAccounts[i] = make([]accounts.Account, 3)
+	}
+
+	// add Prime to orderedBlockClient array at [0]
+	if config.PrimeURL != "" {
+		primeClient, err := ethclient.Dial(config.PrimeURL)
+		if err != nil {
+			fmt.Println("Unable to connect to node:", "Prime", config.PrimeURL)
+		} else {
+			allClients.primeClient = primeClient
+			allClients.primeAvailable = true
 		}
-		i++
-	}
-	nonce, err = primeClient.NonceAt(context.Background(), addr, nil)
-	if err != nil {
-		t.Error(err.Error())
-		t.Fail()
-	}
-	inner_tx := types.InternalTx{ChainID: params.MainnetChainConfig.ChainID, Nonce: nonce, GasTipCap: MINERTIP, GasFeeCap: BASEFEE, Gas: GAS, To: nil, Value: VALUE, Data: contract}
-	tx, err := types.SignTx(types.NewTx(&inner_tx), signer, testKey)
-	if err != nil {
-		t.Error(err.Error())
-		t.Fail()
 	}
 
-	err = primeClient.SendTransaction(context.Background(), tx)
-	if err != nil {
-		t.Error(err.Error())
-		t.Fail()
+	// loop to add Regions to orderedBlockClient
+	// remember to set true value for Region to be mined
+	for i, regionURL := range config.RegionURLs {
+		if regionURL != "" {
+			regionClient, err := ethclient.Dial(regionURL)
+			if err != nil {
+				fmt.Println("Unable to connect to node:", "Region", i+1, regionURL)
+				allClients.regionsAvailable[i] = false
+			} else {
+				allClients.regionsAvailable[i] = true
+				allClients.regionClients[i] = regionClient
+			}
+		}
 	}
 
+	// loop to add Zones to orderedBlockClient
+	// remember ZoneURLS is a 2D array
+	for i, zonesURLs := range config.ZoneURLs {
+		for j, zoneURL := range zonesURLs {
+			if zoneURL != "" {
+				zoneClient, err := ethclient.Dial(zoneURL)
+				if err != nil {
+					fmt.Println("Unable to connect to node:", "Zone", i+1, j+1, zoneURL)
+					allClients.zonesAvailable[i][j] = false
+				} else {
+					allClients.zonesAvailable[i][j] = true
+					allClients.zoneClients[i][j] = zoneClient
+				}
+			}
+		}
+	}
+	return allClients
+}
+
+func addAccToClient(clients *orderedBlockClients, acc accounts.Account, i int) {
+	switch i {
+	case 0:
+		clients.primeAccount = acc
+	case 1:
+		clients.regionAccounts[0] = acc
+	case 2:
+		clients.zoneAccounts[0][0] = acc
+	case 3:
+		clients.zoneAccounts[0][1] = acc
+	case 4:
+		clients.zoneAccounts[0][2] = acc
+	case 5:
+		clients.regionAccounts[1] = acc
+	case 6:
+		clients.zoneAccounts[1][0] = acc
+	case 7:
+		clients.zoneAccounts[1][1] = acc
+	case 8:
+		clients.zoneAccounts[1][2] = acc
+	case 9:
+		clients.regionAccounts[2] = acc
+	case 10:
+		clients.zoneAccounts[2][0] = acc
+	case 11:
+		clients.zoneAccounts[2][1] = acc
+	case 12:
+		clients.zoneAccounts[2][2] = acc
+	default:
+		fmt.Println("Error adding account to client, chain not found " + fmt.Sprint(i))
+	}
 }
